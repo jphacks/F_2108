4fbd3a3a2e8bee6cbaf0aeef69acd276cde17039
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-23 19:12:39 +0900

    setup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("HELLO WORLD")
+import Fastify from "fastify"
+
+const server = Fastify()
+
+server.get("/ping", async (req, res) => {
+  return { pong: "it worked!" }
+})
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 3000
+    await server.listen(PORT, "0.0.0.0")
+    console.log(`listening localhost:${PORT}`)
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
+start()
