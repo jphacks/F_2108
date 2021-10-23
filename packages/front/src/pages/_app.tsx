7ade47afd7db180271b5b999b9218a1f8d6cb539
watanabe-sota <819sota@gmail.com>
2021-10-23 14:03:45 +0900

    add:　tailwindセットアップ

--- packages/front/src/pages/_app.tsx
+++ packages/front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import '../styles/tailwind.css'
 import { NextPage } from "next"
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
