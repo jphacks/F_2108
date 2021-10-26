20082047550e38b2a326580afca2ee7f61f57624
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-26 18:38:48 +0900

    change directory

--- packages/front/src/components/components/PdfViewer/pdf-worker.ts
+++ packages/front/src/components/components/PdfViewer/pdf-worker.ts
@@ -1,5 +1,5 @@
 // NOTE: react-pdfをNext.jsで動作させるために、動的に読み込むスクリプトを変更する（https://codesandbox.io/s/react-pdf-next-js-y4ev2）
-/* eslint-disable no-undef */
+// @ts-ignore
 if (process.env.NODE_ENV === "production") {
   // use minified verion for production
   // @ts-ignore
