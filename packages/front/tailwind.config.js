3dc7903493e35ff31eb4dd6591a28f6dbac42c5d
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-26 18:38:57 +0900

    setup tailwind config

--- packages/front/tailwind.config.js
+++ packages/front/tailwind.config.js
@@ -1,3 +1,5 @@
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
+// eslint-disable-next-line no-undef
 module.exports = {
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +8,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F4E12F",
+        secondary: "#333333",
+      },
+      boxShadow: {
+        main: "4px 8px 4px rgba(0, 0, 0, 0.5)",
+      },
+    },
   },
   variants: {
     extend: {},
