06b2cb3d7aede410e65b4b3a1debe1965e6798cd
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-28 00:19:28 +0900

    s3 public read

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "main" {
   bucket = var.project
-  acl    = "private"
+  acl    = "public-read"
+
+  policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Sid : "",
+        Effect : "Allow",
+        Principal : "*",
+        Action : ["s3:GetObject", "s3:GetObjectVersion"],
+        Resource : ["arn:aws:s3:::${var.project}/*"]
+      }
+    ]
+  })
 
   tags = {
     Project = var.project
