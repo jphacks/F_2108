c0b0abd5fec291e6245e36c3d11e37d8d8059390
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-29 01:50:35 +0900

    feat:dashboard api call

--- packages/front/src/components/organisms/listElement.tsx
+++ packages/front/src/components/organisms/listElement.tsx
@@ -1,11 +1,10 @@
+import { FileDataSnapshot } from "@domain/fileDataSnapshot"
+import Link from "next/link"
 import Image from "next/image"
+import React from "react"
 
 export type ListElementProps = {
-  name: string
-  thumbnail: string
-  editUser: string
-  editDate: string
-  nextPage: () => void
+  file: FileDataSnapshot
 }
 
 /**
@@ -17,43 +16,41 @@ export type ListElementProps = {
  * @param nextPage 詳細ページへ遷移(次ページにquery渡す)
  */
 
-export const ListElement: React.VFC<ListElementProps> = ({
-  name,
-  thumbnail,
-  editDate,
-  editUser,
-  nextPage,
-}) => {
+export const ListElement: React.VFC<ListElementProps> = ({ file }) => {
   return (
-    <button onClick={nextPage}>
-      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
-        <a href="#">
-          <Image
-            src={thumbnail}
-            width={200}
-            height={100}
-            className="object-cover pointer-events-none"
-          />
-        </a>
-        <div className="p-5">
-          <div className="flex">
+    <Link href={`/${file.file.id}`}>
+      <a>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
+          <a href="#">
             <Image
-              src="/play.png"
-              width={35}
-              height={10}
-              className="pointer-events-none"
+              src={file.file.thumbnail}
+              width={200}
+              height={100}
+              className="object-cover pointer-events-none"
             />
-            <div className="flex items-center ml-2">
-              <p className="text-gray-900 font-bold tracking-tight mb-2 mt-2">
-                {name}
-              </p>
+          </a>
+          <div className="p-5">
+            <div className="flex">
+              <Image
+                src="/play.png"
+                width={35}
+                height={10}
+                className="pointer-events-none"
+              />
+              <div className="flex items-center ml-2">
+                <p className="mt-2 mb-2 font-bold tracking-tight text-gray-900">
+                  {file.file.name}
+                </p>
+              </div>
             </div>
+            <p className="mt-3 mb-3 font-normal text-gray-700">
+              {file.updatedBy != null
+                ? `${file.updatedBy?.name} が ${file.updatedAt} に編集`
+                : `${file.file.author.name} が ${file.file.postedAt} に編集`}
+            </p>
           </div>
-          <p className="font-normal text-gray-700 mb-3 mt-3">
-            {editUser} が {editDate} に編集
-          </p>
         </div>
-      </div>
-    </button>
+      </a>
+    </Link>
   )
 }
--- packages/front/src/pages/dashboard/index.tsx
+++ packages/front/src/pages/dashboard/index.tsx
@@ -2,13 +2,26 @@ import { ListElement } from "@components/organisms/listElement"
 import { dummyFileList } from "@mocks/testListDate"
 import { AddButton } from "@components/organisms/addButton"
 import { Modal } from "@components/organisms/modal"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FileUploader } from "@components/organisms/fileUploader"
 import Image from "next/image"
 import { SearchInput } from "@components/organisms/searchInput"
+import { useAuth } from "@hooks/useAuth"
+import { useRouter } from "next/router"
+import { useRequest } from "@hooks/useRequest"
+import { useFile } from "@hooks/useFile"
 
 const Index: React.VFC = () => {
   const [isOpenModal, setIsOpenModal] = useState(false)
+  const user = useAuth()
+  const router = useRouter()
+  const fileUsecase = useFile()
+
+  useEffect(() => {
+    !user && router.push("/login")
+  }, [user])
+
+  const { data: files } = useRequest(() => fileUsecase.fetchFileList(), [])
 
   //TODO:ここのsearchNameを使い配列をfilterする
   const [searchName, setSearchName] = useState<string>("")
@@ -16,8 +29,9 @@ const Index: React.VFC = () => {
   const openModal = () => {
     setIsOpenModal(!isOpenModal)
   }
+
   return (
-    <body className="bg-gray-200 h-screen">
+    <body className="h-screen bg-gray-200">
       <Modal
         isOpen={isOpenModal}
         body={
@@ -27,35 +41,27 @@ const Index: React.VFC = () => {
         }
         onClick={openModal}
       />
-      <header className="text-gray-100 bg-black body-font shadow w-full">
-        <div className="container flex flex-wrap p-4 flex-col md:flex-row items-center ">
+      <header className="w-full text-gray-100 bg-black shadow body-font">
+        <div className="container flex flex-col flex-wrap items-center p-4 md:flex-row ">
           <Image
             src="/logo.png"
             width={180}
             height={80}
-            className="object-fit w-full"
+            className="w-full object-fit"
           />
-          <div className="flex justify-first ml-12">
+          <div className="flex ml-12 justify-first">
             <SearchInput setter={setSearchName} value={searchName} />
           </div>
         </div>
       </header>
-      <div className="ml-20 mt-4">
+      <div className="mt-4 ml-20">
         <AddButton onClick={openModal} />
       </div>
       <div className="mx-auto">
-        <ul className="grid sm:grid-cols-2 xl:grid-cols-3 gap-6 w-2/3 mx-auto">
-          {dummyFileList.map((fl) => (
-            <li key={fl.id} className="mx-auto">
-              <ListElement
-                name={fl.name}
-                thumbnail={fl.thumbnail}
-                editDate={fl.editDate}
-                editUser={fl.editUser}
-                nextPage={() => {
-                  console.log("next_page")
-                }}
-              />
+        <ul className="grid w-2/3 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-3">
+          {files.map((file) => (
+            <li key={file.file.id} className="mx-auto">
+              <ListElement file={file} />
             </li>
           ))}
         </ul>
