961a3365498522cb6dffe3a4bc38fa5aa9bdc3d1
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-29 02:54:24 +0900

    feat:file update

--- packages/front/src/components/atoms/PrimaryButton.tsx
+++ packages/front/src/components/atoms/PrimaryButton.tsx
@@ -1,16 +1,22 @@
 import React from "react"
 
 type PrimaryButtonProps = {
-  onClick: () => void
+  onClick?: () => void
   children: React.ReactNode
+  disabled?: boolean
 }
 
 const PrimaryButton: React.VFC<PrimaryButtonProps> = ({
   children,
   onClick,
+  disabled = false,
 }) => {
   return (
-    <button className="px-4 py-2 text-black rounded-lg bg-primary">
+    <button
+      className="px-4 py-2 text-black rounded-lg bg-primary disabled:bg-gray-200 disabled:text-gray-400"
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   )
--- packages/front/src/components/organisms/fileUploader.tsx
+++ packages/front/src/components/organisms/fileUploader.tsx
@@ -1,10 +1,16 @@
-import { ChangeEvent, useState } from "react"
+import PrimaryButton from "@components/atoms/PrimaryButton"
+import { useFile } from "@hooks/useFile"
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import React, { ChangeEvent, useState } from "react"
 
-export const FileUploader = () => {
-  const [pdf, setPdf] = useState<string>()
-  const [fileName, setFileName] = useState<string>()
+export const FileUploader: NextPage = () => {
+  const router = useRouter()
+  const fileUseCase = useFile()
+  const [pdf, setPdf] = useState<File | null>(null)
+  const [fileName, setFileName] = useState<string>("")
 
-  const handleValueChange = (
+  const handleFileName = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const inputValue = event.target.value
@@ -13,18 +19,34 @@ export const FileUploader = () => {
 
   // ローカルからPDFを追加する
   const imageHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files === null) {
+    // event.preventDefault()
+    if (event.target.files == null) {
       return
     }
     const file = event.target.files[0]
-    if (file === null) {
+    if (file == null) {
       return
     }
-    const reader = new FileReader()
-    reader.readAsDataURL(file as Blob)
-    reader.onload = () => {
-      setPdf(reader.result as string)
+    setPdf(file)
+    // const reader = new FileReader()
+    // reader.readAsDataURL(file as Blob)
+    // reader.onload = () => {
+    //   setPdf(reader.result as string)
+    // }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // e.preventDefault()
+    if (pdf == null) {
+      return
     }
+    console.log("upload")
+    const res = await fileUseCase.uploadFile({
+      file: pdf,
+      name: fileName,
+    })
+
+    router.push(`/${res.file.id}`)
   }
 
   // TODO:アップロード素材
@@ -32,33 +54,31 @@ export const FileUploader = () => {
   console.log(fileName)
   return (
     <>
-      <div className="relative flex items-center justify-center  sm:px-6 relative items-center">
-        <div className="sm:max-w-lg w-full p-10 bg-white rounded-xl z-10">
+      <div className="relative flex items-center justify-center sm:px-6">
+        <div className="z-10 w-full p-10 bg-white sm:max-w-lg rounded-xl">
           <div className="text-center">
             <h2 className="text-3xl font-bold text-gray-900">
               PDF アップロード
             </h2>
           </div>
-          <form className="mt-8 space-y-3" action="#" method="POST">
+          <form className="mt-8 space-y-3" onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 space-y-2">
-              <label className="text-sm font-bold text-gray-500 tracking-wide">
+              <label className="text-sm font-bold tracking-wide text-gray-500">
                 PDF名
               </label>
               <input
-                className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                className="p-2 text-base border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
                 type=""
                 value={fileName}
-                onChange={(e) => {
-                  handleValueChange(e)
-                }}
+                onChange={handleFileName}
                 placeholder="エンジニア議事録"
               />
             </div>
             <div className="grid grid-cols-1 space-y-2">
               <div className="flex items-center justify-center w-full">
-                <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
-                  <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
-                    <p className="pointer-none text-gray-500 ">
+                <label className="flex flex-col w-full p-10 text-center border-4 border-dashed rounded-lg h-60 group">
+                  <div className="flex flex-col items-center justify-center w-full h-full text-center ">
+                    <p className="text-gray-500 pointer-none ">
                       <span className="text-sm">Drag and drop</span> files here{" "}
                       <br /> or{" "}
                       <a
@@ -78,6 +98,11 @@ export const FileUploader = () => {
                   />
                 </label>
               </div>
+            </div>
+            <div className="w-full mt-4 text-center">
+              <PrimaryButton disabled={fileName == "" || pdf == null}>
+                OK
+              </PrimaryButton>
             </div>
           </form>
         </div>
