e8566d36e38d46a481b2a5299faae4c33ab6caa1
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-30 02:05:02 +0900

    add:upload redirect

--- packages/front/src/components/organisms/fileUploader.tsx
+++ packages/front/src/components/organisms/fileUploader.tsx
@@ -40,13 +40,12 @@ export const FileUploader: NextPage = () => {
     if (pdf == null) {
       return
     }
-    console.log("upload")
     const res = await fileUseCase.uploadFile({
       file: pdf,
       name: fileName,
     })
 
-    // router.push(`/${res.file.id}`)
+    router.push(`/${res.file.id}`)
   }
 
   // TODO:アップロード素材
