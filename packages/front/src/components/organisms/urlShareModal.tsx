8e111b6dae41af6131617e6564ca40e7db18d331
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-30 03:03:41 +0900

    fix:remove unused code

--- packages/front/src/components/organisms/urlShareModal.tsx
+++ packages/front/src/components/organisms/urlShareModal.tsx
@@ -53,30 +53,6 @@ export const UrlShareModal: React.VFC<UrlShareModalProps> = ({
             <div className="inline-block w-full max-w-lg my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
               <UrlShare file={file} />
             </div>
-            {/* <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-              <Dialog.Title
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900"
-              >
-                Payment successful
-              </Dialog.Title>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Your payment has been successfully submitted. We’ve sent you
-                  an email with all of the details of your order.
-                </p>
-              </div>
-
-              <div className="mt-4">
-                <button
-                  type="button"
-                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                  onClick={onClose}
-                >
-                  Got it, thanks!
-                </button>
-              </div>
-            </div> */}
           </Transition.Child>
         </div>
       </Dialog>
