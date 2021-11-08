ad45ff955abeca9e747ef189aa35fa02797a1891
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-11-08 22:24:15 +0900

    fix:type error

--- packages/front/src/pages/_document.tsx
+++ packages/front/src/pages/_document.tsx
@@ -6,7 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;700&display=swap"
             rel="stylesheet"
