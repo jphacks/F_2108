493ae3c2079eaa6fa8a7fac894925eba8fb0c519
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-13 18:00:22 +0900

    always use thumbnail generator

--- packages/server/src/handler/file.ts
+++ packages/server/src/handler/file.ts
@@ -140,8 +140,12 @@ export const fileHandler = async (server: FastifyInstance) => {
       },
     })
 
-    server.thumbnailGenerator().generate(result.id, req.body.file.value)
-    fileModel.thumbnail = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/thumbnail/${fileModel.id}`
-    await repository.save(fileModel)
+    try {
+      await server.thumbnailGenerator().generate(result.id, req.body.file.value)
+      fileModel.thumbnail = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/thumbnail/${fileModel.id}`
+      await repository.save(fileModel)
+    } catch (e) {
+      console.error(e)
+    }
   })
 }
